dda9fd595090b258dbb36eaefed3676175fccc74
nusubbotin <nu.subbotin@ya.ru>
2022-09-22 17:03:04 +0300

    Циклы. Часть 1 (ДЗ) Выполнение требований по Заданию №4

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1: ");
-        for (int i = 1 ; i < 11 ; i++){
+        for (int i = 1; i < 11; i++){
             System.out.println("i = " + i);
         }
 
         System.out.println("Задание 2: ");
-        for (int i = 10 ; i > 0 ; i--){
+        for (int i = 10; i > 0; i--){
             System.out.println("i = " + i);
         }
 
         System.out.println("Задание 3: ");
-        for (int i = 0 ; i < 17 ; i = i + 2){
+        for (int i = 0; i < 17; i = i + 2){
+            System.out.println("i = " + i);
+        }
+
+        System.out.println("Задание 4: ");
+        for (int i = 10; i > -11; i--){
             System.out.println("i = " + i);
         }
     }
