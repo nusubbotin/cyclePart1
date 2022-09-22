185e42511f6e473506a4dbf4163eaf3c20697c2c
nusubbotin <nu.subbotin@ya.ru>
2022-09-22 17:38:41 +0300

    Циклы. Часть 1 (ДЗ) Выполнение требований по Заданию Домашнее задание -2. Задача 1

--- src/Main.java
+++ src/Main.java
@@ -19,5 +19,11 @@ public class Main {
         for (int i = 10; i > -11; i--){
             System.out.println("i = " + i);
         }
+
+        //Домашнее задание -2. Задача 1
+        System.out.println("Задание 5: ");
+        for (int i = 1904; i <= 2096; i = i + 4){
+            System.out.println(i + " год является високосным");
+        }
     }
 }
